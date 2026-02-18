8d0a0371d6cc5eaae88ac42ee637a80b9e2d981a
megha0007 <meghashinde7689@gmail.com>
2026-02-18 17:27:27 +0530

    modification

--- nearby_services/config/settings.py
+++ nearby_services/config/settings.py
@@ -78,27 +78,27 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.contrib.gis.db.backends.postgis',
-#         'NAME': 'nearby',
-#         'USER': 'postgres',
-#         'PASSWORD': 'postgres',
-#         'HOST': 'db',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_DB'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': os.environ.get('POSTGRES_HOST'),  # NOT 'db'
-        'PORT': os.environ.get('POSTGRES_PORT', 5432),
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'nearby',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.environ.get('POSTGRES_DB'),
+#         'USER': os.environ.get('POSTGRES_USER'),
+#         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+#         'HOST': os.environ.get('POSTGRES_HOST'),  # NOT 'db'
+#         'PORT': os.environ.get('POSTGRES_PORT', 5432),
+#     }
+# }
 
 
 # Password validation

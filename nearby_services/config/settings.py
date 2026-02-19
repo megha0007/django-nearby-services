a092773e146449218f3801df5847bfd076376873
megha0007 <110972176+megha0007@users.noreply.github.com>
2026-02-19 16:45:58 +0530

    Update settings.py

--- nearby_services/config/settings.py
+++ nearby_services/config/settings.py
@@ -78,16 +78,23 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.contrib.gis.db.backends.postgis',
+#         'NAME': 'nearby',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST': 'db',
+#         'PORT': '5432',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'nearby',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',
-        'PORT': '5432',
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        engine="django.contrib.gis.db.backends.postgis"
+    )
 }
+
 
 # DATABASES = {
 #     'default': {
